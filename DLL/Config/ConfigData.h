d42ff3f27147eaf514fef5af9fcfacdc2d3ecb24
Rodrigo Santos <HansyRod@users.noreply.github.com>
2026-04-02 23:14:22 +0100

    Add support for randomized cups in config file

--- DLL/Config/ConfigData.h
+++ DLL/Config/ConfigData.h
@@ -45,13 +45,37 @@ namespace Randomizer {
     };
     NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(RandomizedTrack, folder, difficulty, obtain)
 
+    // Represents an entry in the "stages" array within a cup
+    struct RandomizedCupStage {
+        std::string trackFolder;
+        int numLaps;
+        bool isReverse;
+        bool isMirror;
+    };
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(RandomizedCupStage, trackFolder, numLaps, isReverse, isMirror)
+
+    struct RandomizedCup {
+        std::string name;
+        int difficulty;
+        Obtain obtainCondition;
+        int numCars;
+        int numTries;
+        int perRaceRequiredPlace;
+        int overallRequiredPlace;
+        std::vector<int> carsPerClass; // Max number of AI allowed from each class (Rookie, Amateur, etc.)
+        std::vector<int> pointsTable; // Points for each position (1st to 16th)
+        std::vector<RandomizedCupStage> stages;
+    };
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(RandomizedCup, name, difficulty, obtainCondition, numCars, numTries, perRaceRequiredPlace, overallRequiredPlace, carsPerClass, pointsTable, stages)
+
     // The root structure containing all randomizer data
     struct ConfigData {
         ConfigMetadata metadata;
         ConfigGlobalOptions global_options;
         std::vector<RandomizedCar> cars;
         std::vector<RandomizedTrack> tracks;
+        std::vector<RandomizedCup> cups;
     };
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ConfigData, metadata, global_options, cars, tracks)
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ConfigData, metadata, global_options, cars, tracks, cups)
 
 } // namespace Randomizer
